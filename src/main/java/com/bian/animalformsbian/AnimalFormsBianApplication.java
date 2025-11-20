7432caafd40aa2e6e1e0d3e12759b398ca172ef3
Leandro Jacome <leandrojacome@wposs.com>
2025-11-20 08:43:42 -0500

    first commit

---
+++ src/main/java/com/bian/animalformsbian/AnimalFormsBianApplication.java
@@ -0,0 +1,13 @@
+package com.bian.animalformsbian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalFormsBianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalFormsBianApplication.class, args);
+    }
+
+}
