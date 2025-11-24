cef0d3cf7a571ef8c77c9c636d43e4a36f32443a
Leandro Jacome <leandrojacome@wposs.com>
2025-11-24 15:14:06 -0500

    Prueba

--- src/main/java/com/bian/animalformsbian/adapter/driving/http/feing/IFeingClient.java
+++ src/main/java/com/bian/animalformsbian/adapter/driving/http/feing/IFeingClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-bian")
+@FeignClient(name = "user-bian", url = "${USER_SERVICE_URL}")
 public interface IFeingClient {
     @GetMapping("/users/document/{document}")
     String getIdByDocument(@PathVariable("document") String document);
